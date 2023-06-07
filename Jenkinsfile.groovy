ef5c567adf9b00620d1f69fc87276e0d9c786a37
Shane Mulcair <jack.p.ryan@gmail.com>
2023-06-07 21:18:14 +0100

    Installation done in dockerfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,6 @@
 pipeline{
     agent any
         stages{
-            stage("Setup"){
-                steps{
-                    sh "apt-get update && apt-get install -y python3-pip"
-                    sh "pip3 install --user docker"
-                }
-            }
             stage("Build"){
                 steps{
                 sh "python3 run.py --container cytopia/dvwa:latest"
