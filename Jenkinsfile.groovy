90397f2abfcf90ddeba8a9dd9beba30fb7f662ee
Shane Mulcair <jack.p.ryan@gmail.com>
2023-06-07 21:05:42 +0100

    Typo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,9 @@ pipeline{
     agent none
         stages{
             stage("Build"){
+                steps{
                 sh "python3 run.py --container cytopia/dvwa:latest"
+                }
             }
         }
     
