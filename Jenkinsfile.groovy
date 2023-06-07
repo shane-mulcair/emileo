82b09023c2fc65b248f7220756f265e4d5b65a59
Shane Mulcair <jack.p.ryan@gmail.com>
2023-06-07 21:11:15 +0100

    Adding pip

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,12 @@
 pipeline{
     agent any
         stages{
+            stage("Setup"){
+                steps{
+                    sh "sudo apt-get update && sudo apt-get install -y python3-pip"
+                    sh "pip3 install --user docker"
+                }
+            }
             stage("Build"){
                 steps{
                 sh "python3 run.py --container cytopia/dvwa:latest"
